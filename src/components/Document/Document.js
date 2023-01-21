098fbc1d3b7d942e325b929d9f8d3506e92213df
Waishnav <waishnavdeore@gmail.com>
2023-01-21 22:55:40 +0000

    added Document Editor and implemented real time collaboration

--- src/components/Document/Document.js
+++ src/components/Document/Document.js
@@ -1,7 +1,9 @@
 import "./Document.css"
 import {useEffect, useCallback, useState} from "react"
 import Quill from "quill"
+import { io } from "socket.io-client"
 import "quill/dist/quill.snow.css"
+import { useParams } from "react-router-dom"
 
 const OPTIONS = [
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -16,6 +18,10 @@ const OPTIONS = [
 ]
 
 function DocumentEditor() {
+  const [title, setTitle] = useState("Untitled Document");
+  const [socket, setSocket] = useState();
+  const [quill, setQuill] = useState();
+  const {id : docID} = useParams();
   function shareDocument() {
     const link = document.location.href;
     navigator.clipboard.writeText(link)
@@ -27,29 +33,83 @@ function DocumentEditor() {
       })
   }
 
+  function handleTitleChange(newTitle) {
+    setTitle(newTitle);
+    socket.emit("update-title", {docID, newTitle});
+  }
+
   const wrapperRef = useCallback((wrapper)=>{
     if (wrapper == null) return
 
     wrapper.innerHTML = ""
     const editor = document.createElement('div')
     wrapper.append(editor)
-    new Quill(editor, {
+    const q = new Quill(editor, {
       theme: "snow",
       modules: {
         toolbar: OPTIONS
       }
     })
+    q.disable()
+    q.setText("Loading...")
+    setQuill(q)
+  }, [])
+
+  useEffect(()=>{
+    const s = io("http://localhost:5000")
+    setSocket(s)
     return () => {
-      wrapperRef.innerHTML = ""
+      s.disconnect()
     }
   }, [])
-  useEffect(()=>{
-    const socket = io("http://localhost:3001")
-  })
+
+  useEffect(() => {
+    if (socket == null) return
+    socket.on("title-updated", title => setTitle(title));
+  }, [socket]);
+
+  useEffect(()=> {
+    if (socket == null || quill == null) return
+
+    socket.once("load-document", document => {
+      quill.setContents(document)
+      quill.enable()
+    })
+
+    socket.emit("get-document", docID)
+  }, [socket, quill, docID])
+
+  useEffect(() => {
+    if (socket == null || quill == null) return
+
+    const handler = delta => {
+      quill.updateContents(delta)
+    }
+    socket.on("receive-changes", handler)
+
+    return () => {
+      socket.off("receive-changes", handler)
+    }
+  }, [socket, quill])
+
+  useEffect(() => {
+    if (socket == null || quill == null) return
+
+    const handler = (delta, oldDelta, source) => {
+      if (source !== "user") return
+      socket.emit("send-changes", delta)
+    }
+    quill.on("text-change", handler)
+
+    return () => {
+      quill.off("text-change", handler)
+    }
+  }, [socket, quill])
+
   return (
     <div>
       <div id="header">
-        <div id="title">Untitled Document</div>
+        <input type="text" value={title} onChange={e => handleTitleChange(e.target.value)} />
         <div id="share">
           <button onClick={() => shareDocument()}>Share</button>
         </div>

cff5b54b88bbe803b1dae0cc53e2b9b5c4db75dc
Waishnav <waishnavdeore@gmail.com>
2023-01-21 22:57:51 +0000

    auth.js context

---
+++ src/context/auth.js
@@ -0,0 +1,66 @@
+import React, { createContext, useState, useEffect } from "react";
+import axios from 'axios';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        const headers = {
+          'Authorization': `Bearer ${token}`
+        }
+        const res = await axios.get('/user', {headers});
+        setUser(res.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+        setIsLoading(false);
+      }
+    }
+    fetchUser();
+  }, []);
+
+  const login = async (email, password) => {
+    try {
+      const res = await axios.post('/login', { email, password });
+      setUser(res.data.user);
+      localStorage.setItem('token', res.data.token);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem('token');
+  }
+
+  const register = async (name, email, password) => {
+    try {
+      const res = await axios.post('/register', { name, email, password });
+      setUser(res.data.user);
+      localStorage.setItem('token', res.data.token);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, isLoading, login, logout, register }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = React.useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error('useAuth must be used within a AuthProvider');
+  }
+  return context;
+};
